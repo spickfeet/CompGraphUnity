8acf2aa26a1feb7a193441cfe62a2e9d582a2cee
spickfeet <slavaspickfeet@mail.ru>
2024-02-13 15:43:15 +0300

    1
    1

---
+++ CompGraph/Assets/Scripts/RayShooter.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+public class RayShooter : MonoBehaviour
+{
+    private Camera _camera;
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(
+            _camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                StartCoroutine(SphereIndicator(hit.point));
+            }
+        }
+    }
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+}
